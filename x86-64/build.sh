28ed3d357b7595a1c38b4f2ca7f1234c5844edf0
storm0606 <hello_zhangyue@126.com>
2025-07-30 00:59:03 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -57,6 +57,8 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-app-mosdns"
+PACKAGES="$PACKAGES luci-app-nikki"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
